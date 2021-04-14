2c9164885206c92a17ecf83afb1ad2508df86875
Chaitali-Shreya <82498553+Chaitali-Shreya@users.noreply.github.com>
2021-04-14 16:54:51 +0530

    Add files via upload

---
+++ cafejavascript.js
@@ -0,0 +1,10 @@
+
+            function slideshow(){
+                var a = document.getElementById("check");
+                if(a.style.display === "none"){
+                    a.style.display="block";
+                }else{
+                    a.style.display="none";
+                }
+            }
+    
